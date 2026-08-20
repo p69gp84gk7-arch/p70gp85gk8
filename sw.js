908514551df93d8d0d659268cb21df38e69afc3f
p69gp84gk7-arch <p69gp84gk7@privaterelay.appleid.com>
2026-08-20 08:06:18 +0200

    Update service worker version to 5.19.0

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
 /* cache intermédiaire ne ferait que masquer l'état réel de la synchro.         */
 /* =========================================================================== */
 
-const SW_VERSION = '5.18.0';
+const SW_VERSION = '5.19.0';
 const SHELL = 'gmao-shell-' + SW_VERSION;
 const LIBS  = 'gmao-libs-'  + SW_VERSION;
 const TILES = 'gmao-tiles-v1';
